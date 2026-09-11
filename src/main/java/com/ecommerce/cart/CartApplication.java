a1ad20468ce871292fc474fdd863921a023b2186
Sushant <sushantac@gmail.com>
2026-09-11 22:07:57 +1000

    fix(config): disable Kafka auto-configuration

--- src/main/java/com/ecommerce/cart/CartApplication.java
+++ src/main/java/com/ecommerce/cart/CartApplication.java
@@ -3,7 +3,7 @@ package com.ecommerce.cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 public class CartApplication {
 
     public static void main(String[] args) {
