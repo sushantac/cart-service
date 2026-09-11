217404dbab26b1e7456884a4d5d19bfa2e9bcf12
Sushant <sushantac@gmail.com>
2026-09-11 22:22:02 +1000

    fix(config): fix KafkaAutoConfiguration import

--- src/main/java/com/ecommerce/cart/CartApplication.java
+++ src/main/java/com/ecommerce/cart/CartApplication.java
@@ -2,6 +2,7 @@ package com.ecommerce.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
 @SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 public class CartApplication {
